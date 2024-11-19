f9a585e118393e918ba09b049b4741d4290a5204
Issygaudet <isabelle.gaudet@ucalgary.ca>
2024-11-19 16:37:50 -0700

    more

--- src/Entity/UserRegistered.java
+++ src/Entity/UserRegistered.java
@@ -1,17 +1,15 @@
 package src.Entity;
 
-public class UserRegistered extends UserOrdinary {
-    private String address;
-    private String creditCard;
-    private String debitCard;
-    private boolean annualFeePaid;
+import java.util.Vector;
 
-    public UserRegistered(String name, String email, String password, String address, String creditCard, String debitCard) {
+public class UserRegistered extends UserOrdinary {
+    private boolean annualFeePaid;
+    private UserBankInfo bankInfo;
+
+    public UserRegistered(String name, String email, String password, UserBankInfo bankInfo) {
         super(name, email, password);
-        this.address = address;
-        this.creditCard = creditCard;
-        this.debitCard = debitCard;
         this.annualFeePaid = false;
+        this.bankInfo = bankInfo;
     }
 
     public void payAnnualFee() {
@@ -21,5 +19,44 @@ public class UserRegistered extends UserOrdinary {
 
     public boolean isAnnualFeePaid() {
         return annualFeePaid;
+    }
+
+    public UserBankInfo getBankInfo() {
+        return bankInfo;
+    }
+
+    public void setBankInfo(UserBankInfo bankInfo) {
+        this.bankInfo = bankInfo;
+    }
+
+    //THE 10% THING
+    public void selectSpecialAccessSeats() {
+        // Implementation to select special access seats for registered users
+        System.out.println("Selecting special access seats for registered users.");
+    }
+
+    @Override 
+    public Vector<Movie> searchMovie(String query) {
+        // Implementation to search for movies with priority access for registered users
+        Vector<Movie> movies = super.searchMovie(query);
+        // Add logic to prioritize movies for registered users
+        System.out.println("Searching for movies with priority access for registered users.");
+        return movies;
+    }
+
+    //already have card info....
+    @Override
+    public void makePayment(Payment payment) {
+        // Implementation to make a payment using registered user's bank info
+        if (this.annualFeePaid) {
+            // Process payment without admin fee
+        } else {
+            // Process payment with admin fee
+        }
+    }
+
+    @Override
+    public void cancelBooking(int bookingId) {
+        // gives 100 refund NOT 85
     }
 }
