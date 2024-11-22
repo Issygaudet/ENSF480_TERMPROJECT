f67d5023748ce612401cf00e56a45f62820499ca
Issygaudet <isabelle.gaudet@ucalgary.ca>
2024-11-22 14:49:03 -0700

    ticketcart

--- src/Entity/TicketCart.java
+++ src/Entity/TicketCart.java
@@ -1,5 +1,22 @@
 package entity;
 
+import java.util.ArrayList;
+
 public class TicketCart {
-    
+    public ArrayList<Ticket> tickets_in_cart = new ArrayList<Ticket>();
+    public float totalPrice = 0;
+
+
+
+    public void addToCart(Ticket ticket) {
+        tickets_in_cart.add(ticket);
+    }
+    public void removeFromCart(Ticket ticket) {
+        tickets_in_cart.remove(ticket);
+    }
+    public ArrayList<Ticket> getTicketsInCart() {
+        return tickets_in_cart;
+    }
+
+
 }
