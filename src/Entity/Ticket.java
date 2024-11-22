9bc7bad2222a5273cbeea14d4f3e30748dbd50af
Issygaudet <isabelle.gaudet@ucalgary.ca>
2024-11-22 14:32:27 -0700

    ticket

--- src/Entity/Ticket.java
+++ src/Entity/Ticket.java
@@ -1,4 +1,66 @@
 package entity;
 public class Ticket {
-    
+    public String ticketID;
+    public Movie movie;
+    public Theatre theatre;
+    public String date;
+    public Showtime showtime;
+    public String seat;
+
+    public Ticket(String ticketID, Movie movie, Theatre theatre, String date, Showtime showtime, String seat) {
+        this.ticketID = ticketID;
+        this.movie = movie;
+        this.theatre = theatre;
+        this.date = date;
+        this.showtime = showtime;
+        this.seat = seat;
+    }
+
+    public String getTicketID() {
+        return ticketID;
+    }
+
+    public void setTicketID(String ticketID) {
+        this.ticketID = ticketID;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public Theatre getTheatre() {
+        return theatre;
+    }
+
+    public void setTheatre(Theatre theatre) {
+        this.theatre = theatre;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Showtime getShowtime() {
+        return showtime;
+    }
+
+    public void setShowtime(Showtime showtime) {
+        this.showtime = showtime;
+    }
+
+    public String getSeat() {
+        return seat;
+    }
+
+    public void setSeat(String seat) {
+        this.seat = seat;
+    }
 }
