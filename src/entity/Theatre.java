ba2ee9d98608f877752de39c293da4612eecab22
Issygaudet <isabelle.gaudet@ucalgary.ca>
2024-12-01 15:57:10 -0700

    added basic documentation+header to most entity files

--- src/entity/Theatre.java
+++ src/entity/Theatre.java
@@ -1,3 +1,10 @@
+// Course: ENSF 480
+// Assignment: Term Project
+// Instructor: Syed Shah
+// Students: L01 - Group 14  (Issy Gaudet, Spiro Douvis, Kamand Ghorbanzadeh, Dylan Wenaas.)  
+// Date Submitted: 2024-12-01
+// Description: This file contains the Theatre class which is used to store information about a theatre.
+
 package entity;
 
 import java.util.ArrayList;
@@ -8,38 +15,73 @@ public class Theatre {
     private String location;
     private List<Movie> movies;  // List to hold movies for this theatre
 
+    /**
+     * CONSTRUCTOR THAT INITIALIZES THE THEATRE WITH THE GIVEN ID, LOCATION, AND MOVIES.
+     * @param theatreId THE ID OF THE THEATRE
+     * @param location THE LOCATION OF THE THEATRE
+     * @param movies THE LIST OF MOVIES FOR THE THEATRE
+     */
     public Theatre(int theatreId, String location, List<Movie> movies) {
         this.theatreId = theatreId;
         this.location = location;
         this.movies = movies;
     }
 
+    /**
+     * CONSTRUCTOR THAT INITIALIZES THE THEATRE WITH THE GIVEN ID AND LOCATION.
+     * @param theatreId THE ID OF THE THEATRE
+     * @param location THE LOCATION OF THE THEATRE
+     */
     public Theatre(int theatreId, String location) {
-    this.theatreId = theatreId;
-    this.location = location;
-    this.movies = new ArrayList<>(); // Initialize an empty list if no movies are provided
+        this.theatreId = theatreId;
+        this.location = location;
+        this.movies = new ArrayList<>(); // Initialize an empty list if no movies are provided
     }
 
+    /**
+     * GETS THE ID OF THE THEATRE.
+     * @return THE ID OF THE THEATRE
+     */
     public int getTheatreId() {
         return theatreId;
     }
 
+    /**
+     * SETS THE ID OF THE THEATRE.
+     * @param theatreId THE ID OF THE THEATRE
+     */
     public void setTheatreId(int theatreId) {
         this.theatreId = theatreId;
     }
 
+    /**
+     * GETS THE LOCATION OF THE THEATRE.
+     * @return THE LOCATION OF THE THEATRE
+     */
     public String getLocation() {
         return location;
     }
 
+    /**
+     * SETS THE LOCATION OF THE THEATRE.
+     * @param location THE LOCATION OF THE THEATRE
+     */
     public void setLocation(String location) {
         this.location = location;
     }
 
+    /**
+     * GETS THE LIST OF MOVIES FOR THE THEATRE.
+     * @return THE LIST OF MOVIES FOR THE THEATRE
+     */
     public List<Movie> getMovies() {
         return movies;
     }
 
+    /**
+     * SETS THE LIST OF MOVIES FOR THE THEATRE.
+     * @param movies THE LIST OF MOVIES FOR THE THEATRE
+     */
     public void setMovies(List<Movie> movies) {
         this.movies = movies;
     }
--- src/entity/Ticket.java
+++ src/entity/Ticket.java
@@ -1,3 +1,10 @@
+// Course: ENSF 480
+// Assignment: Term Project
+// Instructor: Syed Shah
+// Students: L01 - Group 14  (Issy Gaudet, Spiro Douvis, Kamand Ghorbanzadeh, Dylan Wenaas.)  
+// Date Submitted: 2024-12-01
+// Description: This file contains the Ticket class which is used to store information about a ticket.
+
 package entity;
 
 import java.time.Duration;
@@ -15,6 +22,15 @@ public class Ticket {
     private Theatre theatre;
     private Date date;
 
+    /**
+     * CONSTRUCTOR THAT INITIALIZES THE TICKET WITH THE GIVEN DETAILS.
+     * @param ticketID THE ID OF THE TICKET
+     * @param movie THE MOVIE OBJECT
+     * @param theatre THE THEATRE OBJECT
+     * @param date THE DATE OF THE TICKET
+     * @param showtime THE SHOWTIME OBJECT
+     * @param seat THE SEAT OF THE TICKET
+     */
     public Ticket(String ticketID, Movie movie, Theatre theatre, Date date, Showtime showtime, String seat) {
         this.ticketID = ticketID;
         this.movie = movie;
@@ -24,6 +40,14 @@ public class Ticket {
         this.seat = seat;
     }
 
+    /**
+     * CONSTRUCTOR THAT INITIALIZES THE TICKET WITH THE GIVEN DETAILS.
+     * @param movie THE MOVIE OBJECT
+     * @param theatre THE THEATRE OBJECT
+     * @param date THE DATE OF THE TICKET
+     * @param showtime THE SHOWTIME OBJECT
+     * @param seat THE SEAT OF THE TICKET
+     */
     public Ticket(Movie movie, Theatre theatre, Date date, Showtime showtime, String seat) {
         this.ticketID = new Random().nextInt(5) + "";
         this.movie = movie;
@@ -33,54 +57,106 @@ public class Ticket {
         this.seat = seat;
     }
 
+    /**
+     * GETS THE ID OF THE TICKET.
+     * @return THE ID OF THE TICKET
+     */
     public String getTicketID() {
         return ticketID;
     }
 
+    /**
+     * SETS THE ID OF THE TICKET.
+     * @param ticketID THE ID OF THE TICKET
+     */
     public void setTicketID(String ticketID) {
         this.ticketID = ticketID;
     }
 
+    /**
+     * GETS THE SHOWTIME OBJECT.
+     * @return THE SHOWTIME OBJECT
+     */
     public Showtime getShowtime() {
         return showtime;
     }
 
+    /**
+     * SETS THE SHOWTIME OBJECT.
+     * @param showtime THE SHOWTIME OBJECT
+     */
     public void setShowtime(Showtime showtime) {
         this.showtime = showtime;
     }
 
+    /**
+     * GETS THE SEAT OF THE TICKET.
+     * @return THE SEAT OF THE TICKET
+     */
     public String getSeat() {
         return seat;
     }
 
+    /**
+     * SETS THE SEAT OF THE TICKET.
+     * @param seat THE SEAT OF THE TICKET
+     */
     public void setSeat(String seat) {
         this.seat = seat;
     }
 
+    /**
+     * GETS THE MOVIE OBJECT.
+     * @return THE MOVIE OBJECT
+     */
     public Movie getMovie() {
         return movie;
     }
 
+    /**
+     * SETS THE MOVIE OBJECT.
+     * @param movie THE MOVIE OBJECT
+     */
     public void setMovie(Movie movie) {
         this.movie = movie;
     }
 
+    /**
+     * GETS THE THEATRE OBJECT.
+     * @return THE THEATRE OBJECT
+     */
     public Theatre getTheatre() {
         return theatre;
     }
 
+    /**
+     * SETS THE THEATRE OBJECT.
+     * @param theatre THE THEATRE OBJECT
+     */
     public void setTheatre(Theatre theatre) {
         this.theatre = theatre;
     }
 
+    /**
+     * GETS THE DATE OF THE TICKET.
+     * @return THE DATE OF THE TICKET
+     */
     public Date getDate() {
         return date;
     }
 
+    /**
+     * SETS THE DATE OF THE TICKET.
+     * @param date THE DATE OF THE TICKET
+     */
     public void setDate(Date date) {
         this.date = date;
     }
 
+    /**
+     * CHECKS IF THE TICKET CAN BE CANCELED.
+     * @return TRUE IF THE TICKET CAN BE CANCELED, FALSE OTHERWISE
+     */
     public boolean canCancel() {
         ZonedDateTime currentDateTime = ZonedDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
